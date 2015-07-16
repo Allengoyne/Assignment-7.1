85867660da897007dfb4c8fb698053c2ce16606f
Allen Goyne <allen.goyne@gmail.com>
2015-07-15 23:29:14 -0500

    changes

--- js/main.js
+++ js/main.js
@@ -19,7 +19,25 @@ return items.filter(function(item){
  });
 };
 
-//this is question 3
+// this is question 3
+var britMoney = items.filter(function(item){
+ return item.currency_code === "GBP";
+ }).map(function(item){
+    return item.title  +  ' costs '  +  item.price  +  ' GBP ';
+   }).reduce(function(html, item){
+     return html + item;
+   });
+
+
+
+// function getSterling(){
+//   return items.filter(function(item){
+//     return item.currency_code + item.title + item.price;
+//   })
+//   .map(function(item){
+//     return item.currency_code === "GBP";
+//   })
+// }
 
 //this is question 4
 
@@ -28,22 +46,28 @@ var wood = items.filter(function(item){
  var hasWood = item.materials.filter(function(material){
    return material === 'wood';
  });
- return hasWood.length;
+   return hasWood.length;
 }).map(function(item){
- return item.title + ': ' + item.price + "\n";
+  return item.title + ': ' + item.price.toFixed(2) + "\n";
 }).reduce(function(html, item){
- return html + item;
+  return html + item;
 });
+
+
+//question 5
+
+var
 
 ///these are the variables
 var answer1 = getAvg(items).toFixed(2);
 var answer2 = getBetween(12.00, 18.00);
-
+// var answer3 = getSterling("GBP");
 
 
 
 ////to the DOM....
 document.getElementById('answer1').innerHTML = answer1
 document.getElementById('answer2').innerHTML = answer2
+document.getElementById("answer3").innerHTML = britMoney;
 
 document.getElementById("answer4").innerHTML = wood;
